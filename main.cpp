0b8b11f8190186334eec3be8c81592421a1d282e
Thomas <89094230+DarkskullDesigns@users.noreply.github.com>
2022-07-20 22:47:04 +0100

    Starter/Main file upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//Files to use:
+
+#include <iostream>
+#include <string>
+
+//Libraries
+using namespace std;
+
+//Main Function
+int main()
+{
+	int x = 1;
+	float y = 2.1;
+	double z = 3.73648624;
+	string a = "STRING!";
+
+	cout << "Hello, I am learning c++ atm, so this is just a demo!" << endl;
+	cout << "Let's start with some variables (as i'm using 'cout' to do this text)" << endl;
+	cout << "the integer x =" << x <<endl;
+	cout << "the integer y =" << y <<endl;
+	cout << "the integer z =" << z <<endl;
+	cout << "the integer a =" << a <<endl;
+
+	//Wait for Input
+	getchar();
+
+	//End
+	return 0;
+}
