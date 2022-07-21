6970f68345958fab09580179601c4256ab4a177f
Thomas <89094230+DarkskullDesigns@users.noreply.github.com>
2022-07-21 21:46:57 +0100

    V1.1: Operator Update
    I have used the various (beginner) operators: + - * /
    in experiments, and I have some ideas on how to implement them into a formula algorithm. 
    However, before I can do that, I need to learn how to use User Input.

--- main.cpp
+++ main.cpp
@@ -14,12 +14,22 @@ int main()
 	double z = 3.73648624;
 	string a = "STRING!";
 
-	cout << "Hello, I am learning c++ atm, so this is just a demo!" << endl;
+	cout << "Hello, I am learning c++ atm, so this is just a demo!\n" << endl;
+
+	//Variables Experimentation
 	cout << "Let's start with some variables (as i'm using 'cout' to do this text)" << endl;
-	cout << "the integer x =" << x <<endl;
-	cout << "the integer y =" << y <<endl;
-	cout << "the integer z =" << z <<endl;
-	cout << "the integer a =" << a <<endl;
+	cout << "the integer x =" << x << endl;
+	cout << "the integer y =" << y << endl;
+	cout << "the integer z =" << z << endl;
+	cout << "the integer a =" << a << "\n" << endl;
+
+	//Operators Experimentation
+	cout << "Let's start experimenting with operators!" << endl;
+	cout << "2+2=" << 2 + 2 << endl;
+	cout << "2-2=" << 2 - 2 << endl;
+	cout << "2*2=" << 2 * 2 << endl;
+	cout << "2/2=" << 2 / 2 << endl;
+	cout << "2%2=" << 2 % 2 << endl;
 
 	//Wait for Input
 	getchar();
