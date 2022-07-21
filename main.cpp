d7262030df3a23e5c637d1cba0613ec92b88f9d8
Thomas <89094230+DarkskullDesigns@users.noreply.github.com>
2022-07-21 22:11:23 +0100

    V1.2: User Input Update
    So I added the user input functionality for my references. 
    Be warned that you can't put in more than one word, or it bugs out.
    I don't know how to fix that :/

--- main.cpp
+++ main.cpp
@@ -9,14 +9,16 @@ using namespace std;
 //Main Function
 int main()
 {
+	
+
+	cout << "Hello, I am learning c++ atm, so this is just a demo!\n" << endl;
+
+	//Variables Experimentation
 	int x = 1;
 	float y = 2.1;
 	double z = 3.73648624;
 	string a = "STRING!";
 
-	cout << "Hello, I am learning c++ atm, so this is just a demo!\n" << endl;
-
-	//Variables Experimentation
 	cout << "Let's start with some variables (as i'm using 'cout' to do this text)" << endl;
 	cout << "the integer x =" << x << endl;
 	cout << "the integer y =" << y << endl;
@@ -31,8 +33,20 @@ int main()
 	cout << "2/2=" << 2 / 2 << endl;
 	cout << "2%2=" << 2 % 2 << "\n" << endl;
 
+	//User Input
+	cout << "Okay, now lets do some User Inputs!" << endl;
+	string name;
+	int age;
+
+	cout << "What is your first name: " << endl;
+	cin >> name;
+	cout << "What is your age: " << endl;
+	cin >> age;
+	cout << "Your name is: " << name << " and your age is: " << age << "\n" << endl;
+
+
 	//Wait for Input
-	getchar();
+	system("Pause");
 
 	//End
 	return 0;
